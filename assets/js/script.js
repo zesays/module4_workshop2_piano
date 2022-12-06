fcc648d772d5f0519c0cb78b9b5409732cd4a486
zesays <220669@student.glu.nl>
2022-12-06 11:36:59 +0100

    m

---
+++ assets/js/script.js
@@ -0,0 +1,152 @@
+document.getElementById("1").onclick = function(){ // als op de c-toets wordt geklikt
+	var c_toets = document.getElementById("audio_1"); // zoek het c-toets audio-element op
+	c_toets.play(); // speel het audio-element af
+}
+document.getElementById("2").onclick = function(){
+	var d_toets = document.getElementById("audio_2");
+	d_toets.play();
+}
+document.getElementById("3").onclick = function(){
+	var e_toets = document.getElementById("audio_3");
+	e_toets.play();
+}
+document.getElementById("4").onclick = function(){
+	var e_toets = document.getElementById("audio_4");
+	e_toets.play();
+}
+document.getElementById("5").onclick = function(){
+	var e_toets = document.getElementById("audio_5");
+	e_toets.play();
+}
+document.getElementById("6").onclick = function(){
+	var e_toets = document.getElementById("audio_6");
+	e_toets.play();
+}
+document.getElementById("7").onclick = function(){
+	var e_toets = document.getElementById("audio_7");
+	e_toets.play();
+}
+document.getElementById("8").onclick = function(){
+	var e_toets = document.getElementById("audio_8");
+	e_toets.play();
+}
+document.getElementById("9").onclick = function(){
+	var e_toets = document.getElementById("audio_9");
+	e_toets.play();
+}
+document.getElementById("10").onclick = function(){
+	var e_toets = document.getElementById("audio_10");
+	e_toets.play();
+}
+document.getElementById("11").onclick = function(){
+	var e_toets = document.getElementById("audio_11");
+	e_toets.play();
+}
+document.getElementById("12").onclick = function(){
+	var e_toets = document.getElementById("audio_12");
+	e_toets.play();
+}
+document.getElementById("13").onclick = function(){
+	var e_toets = document.getElementById("audio_13");
+	e_toets.play();
+}
+document.getElementById("14").onclick = function(){
+	var e_toets = document.getElementById("audio_14");
+	e_toets.play();
+}
+document.getElementById("15").onclick = function(){
+	var e_toets = document.getElementById("audio_15");
+	e_toets.play();
+}
+document.getElementById("16").onclick = function(){
+	var e_toets = document.getElementById("audio_16");
+	e_toets.play();
+}
+document.getElementById("17").onclick = function(){
+	var e_toets = document.getElementById("audio_17");
+	e_toets.play();
+}
+document.getElementById("18").onclick = function(){
+	var e_toets = document.getElementById("audio_18");
+	e_toets.play();
+}
+document.getElementById("19").onclick = function(){
+	var e_toets = document.getElementById("audio_19");
+	e_toets.play();
+}
+document.getElementById("20").onclick = function(){
+	var e_toets = document.getElementById("audio_20");
+	e_toets.play();
+}
+document.getElementById("21").onclick = function(){
+	var e_toets = document.getElementById("audio_21");
+	e_toets.play();
+}
+document.getElementById("22").onclick = function(){
+	var e_toets = document.getElementById("audio_22");
+	e_toets.play();
+}
+document.getElementById("22").onclick = function(){
+	var e_toets = document.getElementById("audio_22");
+	e_toets.play();
+}
+document.getElementById("23").onclick = function(){
+	var e_toets = document.getElementById("audio_1");
+	e_toets.play();
+}
+document.getElementById("24").onclick = function(){
+	var e_toets = document.getElementById("audio_2");
+	e_toets.play();
+}
+document.getElementById("25").onclick = function(){
+	var e_toets = document.getElementById("audio_3");
+	e_toets.play();
+}
+document.getElementById("26").onclick = function(){
+	var e_toets = document.getElementById("audio_4");
+	e_toets.play();
+}
+document.getElementById("27").onclick = function(){
+	var e_toets = document.getElementById("audio_5");
+	e_toets.play();
+}
+document.getElementById("28").onclick = function(){
+	var e_toets = document.getElementById("audio_6");
+	e_toets.play();
+}
+document.getElementById("29").onclick = function(){
+	var e_toets = document.getElementById("audio_7");
+	e_toets.play();
+}
+document.getElementById("30").onclick = function(){
+	var e_toets = document.getElementById("audio_8");
+	e_toets.play();
+}
+document.getElementById("31").onclick = function(){
+	var e_toets = document.getElementById("audio_9");
+	e_toets.play();
+}
+document.getElementById("32").onclick = function(){
+	var e_toets = document.getElementById("audio_10");
+	e_toets.play();
+}
+document.getElementById("33").onclick = function(){
+	var e_toets = document.getElementById("audio_11");
+	e_toets.play();
+}
+document.getElementById("34").onclick = function(){
+	var e_toets = document.getElementById("audio_12");
+	e_toets.play();
+}
+document.getElementById("35").onclick = function(){
+	var e_toets = document.getElementById("audio_13");
+	e_toets.play();
+}
+document.getElementById("36").onclick = function(){
+	var e_toets = document.getElementById("audio_14");
+	e_toets.play();
+}
+document.getElementById("37").onclick = function(){
+	var e_toets = document.getElementById("audio_16");
+	e_toets.play();
+}
